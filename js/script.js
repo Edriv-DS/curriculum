f18823781db1ba1683ecf00a0f8dd50365be4941
Eduardo Rivero <reduardoj@gmail.com>
2025-03-04 19:03:50 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -99,7 +99,7 @@ document.getElementById("form").addEventListener("submit", function(event) {
 	
 	let formData = new FormData(this); // Captura los datos del formulario
 	
-	fetch("https://github.com/Edriv-DS/curriculum/", {  /*http://localhost:5000/guardar*/
+	fetch("http://localhost:5000/guardar", {  
 		method: "POST",
 		body: formData
     })
